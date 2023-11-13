1c969cebb574baf0ed20eb47f2e3fe0147100034
dogcod <105590774+dogcod@users.noreply.github.com>
2023-11-14 04:23:14 +0900

    Update lhs_Is.c

--- lhs_Is.c
+++ lhs_Is.c
@@ -1,12 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+#include <dirent.h>
 
 int main() {
-    if (execlp("ls", "ls", (char *)NULL) == -1) {
-        perror("execlp");
+    DIR *directory;
+    struct dirent *entry;
+
+    // 현재 디렉터리 열기
+    directory = opendir(".");
+
+    if (directory == NULL) {
+        perror("opendir");
         exit(EXIT_FAILURE);
     }
+
+    // 디렉터리 내용 출력
+    while ((entry = readdir(directory)) != NULL) {
+        printf("%s\n", entry->d_name);
+    }
+
+    // 디렉터리 닫기
+    closedir(directory);
 
     return 0;
 }
