f4e618a48c35488dbed6da281a2ddabe992701a1
dogcod <105590774+dogcod@users.noreply.github.com>
2023-11-20 18:53:44 +0900

    Update lhs_pwd.c

--- lhs_pwd.c
+++ lhs_pwd.c
@@ -2,15 +2,44 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int main() {
-    char cwd[1024];
+#define MAX_INPUT_SIZE 1024
 
-    // 현재 작업 디렉터리 경로 가져오기
+void pwd_command() {
+    char cwd[MAX_INPUT_SIZE];
+
     if (getcwd(cwd, sizeof(cwd)) != NULL) {
         printf("%s\n", cwd);
     } else {
         perror("getcwd");
-        exit(EXIT_FAILURE);
+    }
+}
+
+int main() {
+    char input[MAX_INPUT_SIZE];
+
+    while (1) {
+        // 사용자 입력 받기
+        printf("MyShell> ");
+        if (fgets(input, sizeof(input), stdin) == NULL) {
+            break; // 입력이 더 이상 없으면 종료
+        }
+
+        // 개행 문자 제거
+        input[strcspn(input, "\n")] = '\0';
+
+        // 'exit' 명령을 입력하면 종료
+        if (strcmp(input, "exit") == 0) {
+            break;
+        }
+
+        // 'pwd' 명령어 처리
+        if (strcmp(input, "pwd") == 0) {
+            pwd_command();
+        } else {
+            // 다른 명령어 처리
+            // (실제 프로그램에서는 fork와 exec를 사용하여 외부 명령어 실행이 필요)
+            printf("Command not recognized: %s\n", input);
+        }
     }
 
     return 0;
