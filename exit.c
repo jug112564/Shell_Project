9f52ea9d812faf32d219c10693285579668ec228
dogcod <105590774+dogcod@users.noreply.github.com>
2023-11-27 13:19:00 +0900

    Create exit.c

---
+++ exit.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <dirent.h>
+#include <string.h>
+
+int main(){
+  
+if (strcmp(input, "exit") == 0) {
+            printf("Exiting the program.\n");
+            break;
+        }
+}
